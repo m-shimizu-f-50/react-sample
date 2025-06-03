362351537b1f56de2bb747107c32ec27f04bf8dd
simizu-masato <m.shimizu@trigger4.com>
2025-06-03 23:21:18 +0900

    Todoリスト作成準備

---
+++ src/Todo.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export const Todo = () => {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<h1>React表示確認</h1>
+		</>
+	);
+};
+
+export default Todo;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Todo } from './Todo';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement!);
+root.render(
+	<StrictMode>
+		<Todo />
+	</StrictMode>
+);
+
+// createRoot(document.getElementById('root')!).render(
+// 	<StrictMode>
+// 		<App />
+// 	</StrictMode>
+// );
